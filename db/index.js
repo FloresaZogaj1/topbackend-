538ba6be58c9b3a311447df3353afd607ee9bc93
IllyrianMarketing <marketingillyrian@gmail.com>
2025-08-27 11:31:25 +0200

    changes in server file

--- db/index.js
+++ db/index.js
@@ -1,15 +1,21 @@
+// db/index.js
 const mysql = require('mysql2/promise');
+
+const ssl =
+  process.env.DB_SSL === 'true'
+    ? { minVersion: 'TLSv1.2', rejectUnauthorized: false } // ⬅️ lejo pa CA (provizore)
+    : undefined;
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT || 3306),
+  ssl,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: true } : undefined
 });
 
 module.exports = pool;
